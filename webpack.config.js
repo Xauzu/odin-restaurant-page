5379c97165cefab26dedd8a2bd5f7c9af6685016
Xau <frozzy.xfs@gmail.com>
2023-07-24 21:08:38 -0400

    Fix scaling issue, fix tab stacking

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  devtool: 'inline-source-map',
   module: {
     rules: [
       {
